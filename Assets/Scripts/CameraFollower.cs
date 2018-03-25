37a8f03ab9477a8cf8af5a492f7524c895d6fa7a
Dmitrii <timesburnersteam@gmail.com>
2018-03-25 15:30:12 -0700

    `12
    123

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour 
+{
+	
+	public GameObject Player;
+	private Vector3 CameraFollow;
+
+	void Start () 
+	{
+		CameraFollow = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = Player.transform.position  + CameraFollow;
+	}
+}
