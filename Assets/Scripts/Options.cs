e0295611173d35253cd7419b12d2e1ed59ea1205
Dmitrii <timesburnersteam@gmail.com>
2018-03-25 15:43:02 -0700

    123

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    // Need finish
+}
---
+++ Assets/Scripts/Oscillator2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator2 : MonoBehaviour 
+{
+
+	public Vector3 movementVector = new Vector3(10f, 10f);
+	public float period = 2f;
+	public float rotationSpeed;
+	public float xyi = 1f;
+
+	float movementFactor; // 0 for not moved, 1 for fully moved.
+	Vector3 startingPos;
+	// Use this for initialization
+	void Start () 
+	{
+		startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (period <= Mathf.Epsilon) { return; }
+		float cycles = (Time.time + xyi) / period;
+
+		const float tau = Mathf.PI * 2;
+		float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+		movementFactor = rawSinWave / 2f + 0.5f;
+		Vector3 offset = movementFactor * movementVector;
+		transform.position = startingPos + offset;
+
+		float rotation = rotationSpeed * Time.deltaTime;
+		transform.Rotate(Vector3.forward * rotation);
+	}
+}
